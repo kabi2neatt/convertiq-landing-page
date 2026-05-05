e9fc02e1dd58796c1a175b4e81d90913d117e4ea
kabi2neatt <kabir@convert-iq-media.com>
2026-05-05 09:58:50 -0400

    Update ContactForm.tsx

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -140,7 +140,7 @@ export function ContactForm() {
               />
 
               <Image
-                src="Convertiqmedia.png"
+                src="/convertiqmedia.png"
                 alt="ConvertIQ Media"
                 width={400}
                 height={420}
